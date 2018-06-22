457e18dd76e86c99b2e47f6c0ca5a2d75000d036
Josef Micka <josef.micka@gmail.com>
2018-06-22 10:13:40 +0200

    omni cleanup (lib)

--- tweaks/omnimatter/1_update.lua
+++ tweaks/omnimatter/1_update.lua
@@ -2,54 +2,44 @@ if mods["omnimatter_science"] and (mods["omnimatter_crystal"]  or mods["omnimatt
 	sctm.debug(serpent.block(data.raw.tool["omni-pack"]))
 	sctm.debug(serpent.block(data.raw.recipe["omni-pack"]))
 	if data.raw.tool["omni-pack"] and data.raw.recipe["omni-pack"] then
-		-- remove omnipack from all labs
-		for _i, lab in pairs(data.raw["lab"]) do
-			if lab.inputs then
-				for _j, inputs in pairs(lab.inputs) do
-					if inputs == "omni-pack" then
-						table.remove(lab.inputs, _j)
-						break
-					end
-				end
-			end
-		end
-		table.insert(data.raw["lab"]["sct-lab-t2"].inputs, "omni-pack")
-		table.insert(data.raw.technology["sct-lab-t3"].prerequisites, "sct-research-omni")
-		table.insert(data.raw["lab"]["sct-lab-t3"].inputs, "omni-pack")
-		table.insert(data.raw["lab"]["sct-lab-t4"].inputs, "omni-pack")	
-		if data.raw.tool["omni-pack"] then
-			if settings.startup["sct-hd-icons"] and settings.startup["sct-hd-icons"].value == true then
-				data.raw.tool["omni-pack"].icons =
+		-- remove omnipack from basic lab
+		sctm.lab_input_remove("lab", "omni-pack")
+		-- remove omnipack from aai burner lab		
+		sctm.lab_input_remove("burner-lab", "omni-pack")
+		-- add omnipack to sct labs
+		sctm.lab_input_add("sct-lab-t2", "omni-pack")
+		sctm.lab_input_add("sct-lab-t3", "omni-pack")
+		sctm.lab_input_add("sct-lab-t4", "omni-pack")
+		-- add omnipack to bob labs
+		sctm.lab_input_add("lab-2", "omni-pack")
+		sctm.lab_input_add("lab-alien", "omni-pack")
+		-- add t3 lab dep on omnipack research (because it reuqires it)
+		sctm.tech_dependency_add("sct-lab-t3", "sct-research-omni")
+		-- omnipack icons
+		if settings.startup["sct-hd-icons"] and settings.startup["sct-hd-icons"].value == true then
+			data.raw.tool["omni-pack"].icons =
+			{
 				{
-					{
-						icon_size = 64,
-						icon = "__ScienceCostTweakerM__/graphics/omni/omni-pack-64.png",
-					},
-				}
-			else
-				data.raw.tool["omni-pack"].icons =
+					icon_size = 64,
+					icon = "__ScienceCostTweakerM__/graphics/omni/omni-pack-64.png",
+				},
+			}
+		else
+			data.raw.tool["omni-pack"].icons =
+			{
 				{
-					{
-						icon_size = 32,
-						icon = "__ScienceCostTweakerM__/graphics/omni/omni-pack.png",
-					},
-				}
-			end
+					icon_size = 32,
+					icon = "__ScienceCostTweakerM__/graphics/omni/omni-pack.png",
+				},
+			}
 		end
 		-- fix missing omnipack in bobs alien lab
-		if data.raw.lab["lab-alien"] then
-			local omniin = false
-			for _j, input in pairs (data.raw.lab["lab-alien"].inputs) do
-				if input == "omni-pack" then
-					omniin = true
-					break
-				end
-			end
-			if omniin == false then
-				table.insert(data.raw.lab["lab-alien"].inputs, "omni-pack")
-			end
-		end
+		-- add crystalonics deps
+		sctm.tech_dependency_add("sct-research-t3", "crystallonics-1")
+		sctm.tech_dependency_add("sct-research-prod", "crystallonics-2")
+		sctm.tech_dependency_add("sct-research-logistic", "crystallonics-2")
 	else
+		-- disable omni pack research if not present
 		data.raw.technology["sct-research-omni"].enabled = false
 	end
 end
