f8564bec61db26b34a29514c868fa84992da8606
Josef Micka <josef.micka@gmail.com>
2019-05-28 01:53:02 +0200

    py enabled

--- data-updates.lua
+++ data-updates.lua
@@ -1,6 +1,7 @@
 require("config")
 require("tweaks.productivity-limitations")
 -- Technology tweaks
+require("prototypes/1_recipe")
 require("prototypes/1_technology")
 -- mod tweaks
 require("tweaks.aai.1_update")
@@ -8,5 +9,5 @@ require("tweaks.angelsmods.1_update")
 require("tweaks.bobsmods.1_update")
 require("tweaks.omnimatter.1_update")
 
---require("tweaks.pymods.1_update")
+require("tweaks.pymods.1_update")
 --require("tweaks.xander.1_update")
--- data.lua
+++ data.lua
@@ -17,7 +17,7 @@ require("tweaks.angelsmods.0_initial")
 require("tweaks.bobsmods.0_initial")
 require("tweaks.omnimatter.0_initial")
 
---require("tweaks.pymods.0_initial")
+require("tweaks.pymods.0_initial")
 
 
 if (settings.startup["sct-tier1-lab"].value == "lab") then
