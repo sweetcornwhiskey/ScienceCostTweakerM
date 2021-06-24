f3a7aaf9ee7192985951944676f69d2e18c80268
Josef Micka <josef.micka@gmail.com>
2021-06-25 00:21:42 +0200

    #70 use brass in t3 lab when angel smelting present

--- tweaks/angelsmods/1_update.lua
+++ tweaks/angelsmods/1_update.lua
@@ -4,3 +4,9 @@ if mods["angelsbioprocessing"] then
 	sctm.tech_dependency_add("bio-desert-farming", "sct-bio-science-pack")
 	sctm.lab_input_add("lab-2", "sct-bio-science-pack")
 end
+
+if mods["angelssmelting"] then
+	if data.raw.item["brass-alloy"] then
+		sctm.recipe_ingredient_replace("sct-lab3-construction", "steel-plate", "brass-alloy")
+	end
+end
