e23508db328e01f38332f31856a42f5beda9278e
sweetcornwhiskey <122855135+sweetcornwhiskey@users.noreply.github.com>
2023-01-16 21:50:50 -0800

    Update 1_recipe.lua

--- prototypes/1_recipe.lua
+++ prototypes/1_recipe.lua
@@ -1,7 +1,7 @@
+sctm.tech_unlock_add("sct-lab-t3", "sct-t3-flash-fuel2")
+
 if data.raw.fluid["heavy-oil"] and data.raw.fluid["light-oil"] then
-    sctm.hide_recipe("sct-t3-flash-fuel2")
     sctm.tech_unlock_add("sct-lab-t3", "sct-t3-flash-fuel")
 else
     sctm.hide_recipe("sct-t3-flash-fuel")
-    sctm.tech_unlock_add("sct-lab-t3", "sct-t3-flash-fuel2")
 end
